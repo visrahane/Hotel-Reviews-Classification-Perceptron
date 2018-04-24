ffe4d494f830263dc703da6e469f22e73e7a54ff
visrahane <rahane@usc.edu>
2018-04-23 18:32:13 -0700

    implement avg perceptron

--- perceplearn3.py
+++ perceplearn3.py
@@ -2,6 +2,7 @@ import sys;
 import math;
 import json;
 import string;
+import random;
 from collections import defaultdict;
 
 # list ds - {id,class1,class2,map of wordCount}
@@ -13,8 +14,8 @@ def readFile(fileName):
     return inputFileObj;
 
 
-def saveToFile(class1WeightMap,biasClass1,class2WeightMap,biasClass2):
-    f = open("vanillamodel.txt", "w");
+def saveToFile(fileName,class1WeightMap,biasClass1,class2WeightMap,biasClass2):
+    f = open(fileName, "w");
     f.write(json.dumps(class1WeightMap));
     f.write("\n");
     f.write(json.dumps(biasClass1));
@@ -22,7 +23,6 @@ def saveToFile(class1WeightMap,biasClass1,class2WeightMap,biasClass2):
     f.write(json.dumps(class2WeightMap));
     f.write("\n");
     f.write(json.dumps(biasClass2));
-
     f.close()
 
 
@@ -53,38 +53,69 @@ def constructTokens(inputFileObj):
         sentenceFeatureClassList.append(sentenceFeatureClassListRow);
         i += 1;
 
-def calculateWordProbs(classWordMap):
-    print("hi");
 
+def trainAvgPerceptron(classNo):
+    # init
+    classWeightMap = defaultdict(int);
+    cachedWeightMap = defaultdict(int);
 
-def trainPerceptron(classNo):
-    #init
+    # for key in vocabSet:
+    #     classWeightMap[key]=0
+    bias = 0;
+    cachedBias = 0;
+    c = 1;
+    for iteration in range(21):
+        random.shuffle(sentenceFeatureClassList);
+        for doc in sentenceFeatureClassList:
+            activation = 0;
+            for featureX, input in doc[3].items():
+                activation += input * classWeightMap.get(featureX, 0);
+            activation += bias;
+            y = doc[classNo];
+            if (y * activation <= 0):
+                for featureA, inputA in doc[3].items():
+                    classWeightMap[featureA] = classWeightMap.get(featureA, 0) + y * inputA;
+                    cachedWeightMap[featureA] = classWeightMap.get(featureA, 0) + y * inputA;
+                bias += y;
+                cachedBias += y * c;
+            c += 1;
+
+    for key in classWeightMap:
+        classWeightMap[key] -= cachedWeightMap[key] / c;
+
+    return classWeightMap, bias - cachedBias / c;
+
+def trainVanillaPerceptron(classNo):
+    # init
     classWeightMap = defaultdict(int);
     # for key in vocabSet:
     #     classWeightMap[key]=0
-    bias=0;
+    bias = 0;
 
     for iteration in range(21):
         for doc in sentenceFeatureClassList:
-            activation=0;
-            for featureX,input in doc[3].items():
-                activation+=input*classWeightMap.get(featureX,0);
-            activation+=bias;
-            y=doc[classNo];
-            if(y*activation<=0):
-                for featureA,inputA in doc[3].items():
-                    classWeightMap[featureA]=classWeightMap.get(featureA,0)+y*inputA;
+            activation = 0;
+            for featureX, input in doc[3].items():
+                activation += input * classWeightMap.get(featureX, 0);
+            activation += bias;
+            y = doc[classNo];
+            if (y * activation <= 0):
+                for featureA, inputA in doc[3].items():
+                    classWeightMap[featureA] = classWeightMap.get(featureA, 0) + y * inputA;
                 bias += y;
 
-    return classWeightMap,bias;
+
+    return classWeightMap, bias;
 
 inputFileObj = readFile(sys.argv[1]);
 stopList = [line.rstrip() for line in open("input/stop-words.txt", encoding="utf8")];
 constructTokens(inputFileObj);
-class1WeightMap,biasClass1=trainPerceptron(1);
-class2WeightMap,biasClass2=trainPerceptron(2);
-saveToFile(class1WeightMap,biasClass1,class2WeightMap,biasClass2);
-
+class1WeightMap,biasClass1=trainVanillaPerceptron(1);
+class2WeightMap,biasClass2=trainVanillaPerceptron(2);
+class1AvgWeightMap,biasAvgClass1=trainAvgPerceptron(1);
+class2AvgWeightMap,biasAvgClass2=trainAvgPerceptron(2);
+saveToFile("averagedmodel.txt",class1AvgWeightMap,biasAvgClass1,class2AvgWeightMap,biasAvgClass2);
+saveToFile("vanillamodel.txt",class1WeightMap,biasClass1,class2WeightMap,biasClass2);
 # print("classCountMap:",classCountMap);
 # print("priorClassProps:",priorClassProps);
 # print("vocab:",vocabMap);
